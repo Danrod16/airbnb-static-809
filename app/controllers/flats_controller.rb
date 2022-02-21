60498732ef3725173aba73e5b45a78bc32b54fc0
Daniel Rodriguez <daniel.rod93@gmail.com>
2022-02-21 17:40:47 +0800

    Feat: added front end and content to index page / flats

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,4 +1,7 @@
+require "open-uri"
 class FlatsController < ApplicationController
   def index
+    url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
+    @flats = JSON.parse(open(url).read)
   end
 end
